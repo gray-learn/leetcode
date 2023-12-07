486ce4e012dbceae5c822dfd86739ba41e7a5274
gray <graylearnone@gmail.com>
2023-12-06 23:53:19 -0500

    206.reverse-linked-list.java

--- 206.reverse-linked-list.java
+++ 206.reverse-linked-list.java
@@ -16,11 +16,14 @@
  * }
  */
 class Solution {
+    // global variable
+    ListNode newHead = null;
+
     public ListNode reverseList(ListNode head) {
         if(head == null || head.next == null){
             return head;
         }
-        return reverse(head, null);
+        return reverse(head, null); // head, newHead
     }
     private ListNode reverse(ListNode head, ListNode newHead){
         if(head == null){
@@ -30,6 +33,48 @@ class Solution {
         head.next = newHead;
         return reverse(next, head);
     }
+    // TC O(n) SC:O(n)
+
+    // // iterative
+    // public ListNode reverseList(ListNode head) {
+    //     if(head==null || head.next == null)
+    //         return head;
+    //     ListNode curr = head;
+    //     ListNode prev = null;
+    //     while(curr != null){
+    //         ListNode tmp = curr.next;
+    //         // p c t
+    //         curr.next = prev; // c t p
+    //         prev = curr; // c t p 
+    //         curr = tmp; // t c p
+    //     }
+    //     return prev;
+    // }
+    // // TC O(n) SC O(1)
+
+    // recursive
+    // public ListNode reverseList(ListNode head) {
+    //     if(head==null || head.next == null)
+    //         return head;
+    //     reverse(head);
+    //     return newHead;
+    // }
+    // private ListNode reverse(ListNode head){
+    //     if(head.next == null){
+    //         newHead = head;
+    //         return head;
+    //     }
+    //     ListNode tmp = reverse(head.next);
+    //     tmp.next = head;
+    //     head.next = null;
+    //     return head;
+    // }
+    // 1 > 2 > 3 >4
+    // null <1 <2<3<4
+
+
+    // O(n)
+
 
 
 }
