e4b66e8ff585d9f3cfbdc8b0a372100674c7d9ac
gray <graylearnone@gmail.com>
2023-12-06 23:59:36 -0500

    [735] Asteroid Collision

---
+++ 735.asteroid-collision.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=735 lang=java
+ *
+ * [735] Asteroid Collision
+ */
+
+// @lc code=start
+
+import java.util.Stack;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+
+
+        Stack<Integer> st = new Stack<>();
+        for(int asteroid : asteroids){
+            if(asteroid > 0){
+                st.push(asteroid);
+            } else {
+                while(!st.isEmpty() && st.peek() > 0 && Math.abs(asteroid) > st.peek()){
+                    st.pop();
+                }
+                if(st.isEmpty() || st.peek() <0){
+                    st.push(asteroid);
+                }else if(st.peek() + asteroid == 0){
+                    st.pop();
+                }
+            }
+        }
+        int[] ans = new int[st.size()];
+        for(int i = ans.length -1; i>=0 ; i--){
+            ans[i] = st.pop();
+        }
+        return ans;        
+    }
+}
+// @lc code=end
+
