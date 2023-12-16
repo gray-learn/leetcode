65f5d81c80b8bb288d493006608bc227562c37d4
gray <graylearnone@gmail.com>
2023-12-15 23:48:25 -0500

    704.binary-search.java

---
+++ 704.binary-search.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=704 lang=java
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length -1;
+        while(left<=right){
+            int mid = left +(right - left)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(target > nums[mid]){
+                left = mid +1;
+            }
+            else{
+                right = mid -1;
+            }
+        }
+        return -1;
+        
+    }
+}
+// @lc code=end
+
